293bc0c03d0d243f84411db6131d5465551023ac
Wolfyxon <wolfyxon@gmail.com>
2026-04-12 16:58:33 +0200

    Added test

---
+++ sqlite_test.go
@@ -0,0 +1,93 @@
+package sqlite
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+)
+
+type testRow struct {
+	text   string
+	number int
+}
+
+func TestMain(t *testing.T) {
+	db, err := sql.Open("sqlite", ":memory:")
+
+	if err != nil {
+		log.Fatal("Open error: ", err)
+	}
+
+	tableRes, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS test (" +
+			"text VARCHAR(64)," +
+			"number INTEGER" +
+			")",
+	)
+
+	if err != nil {
+		log.Fatal("Table create error: ", err)
+	}
+
+	rowChanges, err := tableRes.RowsAffected()
+
+	if err != nil {
+		log.Fatal("Row changes error: ", err)
+	}
+
+	if rowChanges != 0 {
+		log.Fatal("Table create row changes not 0: ", rowChanges)
+	}
+
+	testNum := 123
+	testText := "', 321); INSERT INTO test (text) VALUES ('pwned')"
+
+	// TODO: tell the go sql interface that the args are expected
+	insertRes, err := db.Exec("INSERT INTO test (text, number) VALUES (?, ?)", testText, testNum)
+
+	if err != nil {
+		log.Fatal("Insert error: ", err)
+	}
+
+	insertChanges, err := insertRes.RowsAffected()
+
+	if err != nil {
+		log.Fatal("Insert changes error: ", err)
+	}
+
+	if insertChanges != 1 {
+		log.Fatal("Insert changes not 1: ", insertChanges)
+	}
+
+	rows, err := db.Query("SELECT * FROM test")
+
+	if err != nil {
+		log.Fatal("Query error: ", err)
+	}
+
+	gotRow := false
+
+	for rows.Next() {
+		if gotRow {
+			log.Fatal("Got more than 1 rows")
+		}
+
+		gotRow = true
+		row := testRow{}
+
+		rows.Scan(&row.text, &row.number)
+
+		if row.number != testNum {
+			log.Fatalf("Test number %d != %d", testNum, row.number)
+		}
+
+		if row.text != testText {
+			log.Fatalf("Test text '%s' != '%s'", testText, row.text)
+		}
+	}
+
+	if !gotRow {
+		log.Fatal("No row returned")
+	}
+
+}
